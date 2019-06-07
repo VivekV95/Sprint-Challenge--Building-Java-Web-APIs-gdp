e57c30c9c82f45caac9cb738882a2858219421b4
VivekV95 <vivekv210@gmail.com>
2019-06-07 10:28:01 -0500

    Initial commit

---
+++ src/main/kotlin/com/vivekvishwanath/sprint12/Sprint12Application.kt
@@ -0,0 +1,11 @@
+package com.vivekvishwanath.sprint12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sprint12Application
+
+fun main(args: Array<String>) {
+    runApplication<Sprint12Application>(*args)
+}
