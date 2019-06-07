35531da2d630f2246bfeea817039d1247c9fe9a7
VivekV95 <vivekv210@gmail.com>
2019-06-07 11:29:12 -0500

    Added ValidationError and ErrorDetail data classes

---
+++ src/main/kotlin/com/vivekvishwanath/sprint12/model/ErrorDetail.kt
@@ -0,0 +1,16 @@
+package com.vivekvishwanath.sprint12.model
+
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
+
+data class ErrorDetail(var title: String? = null, var status: Int? = null, var detail: String? = null,
+                       var developerMessage: String? = null,
+                       var errors: HashMap<String, List<ValidationError>> = HashMap()) {
+    var timestamp: String? = null
+
+
+    fun setTimestamp(value: Long) {
+        timestamp = SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date(value))
+    }
+}
---
+++ src/main/kotlin/com/vivekvishwanath/sprint12/model/ValidationError.kt
@@ -0,0 +1,3 @@
+package com.vivekvishwanath.sprint12.model
+
+data class ValidationError(var code: String? = null, var message: String? = null)
